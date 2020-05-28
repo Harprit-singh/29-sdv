5a71cd102c53ae84f8dab0abc04074273e20c020
Harprit singh <55721002+Harprit-singh@users.noreply.github.com>
2020-05-29 11:17:43 +1200

    Calculator

---
+++ inde.js
@@ -0,0 +1,25 @@
+/*
+class Student {
+    constructon(firstname, lastname){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.year = year; 
+    }
+ fullName() {
+    return 'Your fullName is ${this.firstname}${this.lastname}';
+}
+}
+let firstStudent= new Student("Ali","Kahwaji");
+
+
+console.log(firstStudent.fullName());
+*/
+
+class Calculator{
+    add(a,b){
+        return a+b;
+    
+    }
+    subtract(a, b){
+    return a -b;  
+}
